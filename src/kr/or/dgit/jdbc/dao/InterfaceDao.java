6b46be641025a3ba927aa9ad883b0dd4050322f8
LJH <LJH@DESKTOP-juno>
2016-12-21 14:50:51 +0900

    InterfaceDao<G> create

---
+++ src/kr/or/dgit/jdbc/dao/InterfaceDao.java
@@ -0,0 +1,17 @@
+package kr.or.dgit.jdbc.dao;
+
+import java.util.List;
+
+public interface InterfaceDao<T> {
+	void insertItem(T item);
+
+	void deleteItem(int idx);
+
+	void updateItem(T item);
+
+	T selectByNo(int idx);
+
+	List<T> selectByAll();
+	
+	
+}
